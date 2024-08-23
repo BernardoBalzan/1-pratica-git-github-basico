8082530ef9ae22e0960937ba9cabc586a514f007
BERNARDO BALZAN <b.balzan@edu.pucrs.br>
2024-08-23 19:03:33 -0300

    metodo c) implementado

--- Main.java
+++ Main.java
@@ -16,4 +16,18 @@ public class Main {
         return contador;
     }
 
+    public int nroRepeat(ArrayList< Integer > l) {
+        int aux1 = 0;
+        int repetidos = 0;
+        for (int i = 0; i < l.size(); i++) {
+            aux1 = l.get(i);
+            for (int j = 0; j < l.size(); j++) {
+                if (aux1 == l.get(j)) {
+                    repetidos++;
+                }
+            }
+        }
+        return repetidos;
+    }
+
 }
